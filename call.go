79703d5e931cfcf111529fe9c3ea12fd5b06c6d2
kawasaki8901 <static.average@gmail.com>
2025-02-22 22:03:50 +0900

    エントリポイントを分離した。

---
+++ call.go
@@ -0,0 +1,8 @@
+package test_go_lib
+
+import "fmt"
+
+// Call prints a call message
+func Call() {
+	fmt.Println("Call from test_go_lib!")
+}
--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/kawasaki8901/test_go_lib"
 	"github.com/kawasaki8901/test_go_lib/lib"
 )
 
 func main() {
-	fmt.Printf("call my lib\n")
-	lib.Hello()
-}
-
-func Call() {
-	fmt.Printf("call my lib\n")
+	fmt.Println("Hello, World!")
+	test_go_lib.Call()
 	lib.Hello()
 }
